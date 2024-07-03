97bc3bd9f8c56d6e38981c18623cfc0a377d1c33
Ravisharma-1999 <ravi829144@gmail.com>
2024-07-03 16:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+() => {
+    cy.visit(baseUrl + "/main.html");
+    cy.get('div').then(ans => {
+        let text = ans.text().trim().replace(/[\.,]/g, ''); // Remove commas and periods
+        let expectedText = 'This is a demo of box model try to replicate this box model in Html and Css and try to pass all the test cases.';
+        expect(text).to.equal(expectedText);
+    });
+}
