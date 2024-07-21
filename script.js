2c90aafba8385bae3a61353323e0ebe3819cb571
Ravisharma-1999 <ravi829144@gmail.com>
2024-07-21 19:21:59 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-() => { 
-  cy.visit(baseUrl + "/main.html"); 
-  cy.get('div').then(elem => { 
-    let text = elem.text().trim(); 
-    expect(text).to.eq('This is a demo of box model try to replicate this box model in Html and Css and pass all the test cases.'); 
-  }); 
-}
